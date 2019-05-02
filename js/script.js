bacd5a6a1daa080b60465a6cebd7bdc56c3ea9d3
Gabriel Pereira <gabriel6562014@gmail.com>
2019-05-02 03:33:50 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    load();
+}
+
+function load() {
+    //remove and add your functions
+    console.log('basic website template\ngithub: https://github.com/Gabriel6562014/model\n-by Gabriel.');
+}
