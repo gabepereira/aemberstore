6135f19d8594448d41322e9cd9d94f7b24246bbc
Gabriel Pereira <gabriel6562014@gmail.com>
2019-05-05 19:28:40 -0300

    Signin page created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 const api = 'https://warm-caverns-31460.herokuapp.com/';
+const home = 'https://aemberstore.com';
---
+++ js/signin.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+    load();
+}
+
+let load = () => {
+    let email = document.getElementById('email');
+    let password = document.getElementById('pass');
+    let btn = document.getElementById('signin-btn');
+    let signinWrapper = document.getElementById('signin-wrapper');
+    let result = document.getElementById('result');
+
+    btn.addEventListener('click', function(e) {
+        authCustomer(
+            email.value,
+            password.value
+        ).then(function(response) {
+            console.log(response);
+            sessionStorage.setItem('token', response.token);
+            sessionStorage.setItem('name', response.data.name);
+            window.location = home;
+        }).catch(function(e) {
+            console.log(e);
+            result.classList.remove('none');
+            result.innerHTML = 'Error: ' + e;
+        });
+    });
+}
+
+let authCustomer = async (email, password) => {
+    let response;
+    await fetch(api + 'customer/auth/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    }).then(async function(data) {
+        response = await data.json();
+    }).catch(function(e) {
+        error(e);
+    });
+    return response;
+}
