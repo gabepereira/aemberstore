b401569fa5d67f3af18da9e025ec14e98da5615b
Gabriel Pereira <gabriel6562014@gmail.com>
2019-06-02 01:05:55 -0300

    First production build

---
+++ build/precache-manifest.8f0efd4268e6ee05b3afc1195e7944f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd3f6d756a256f1b438341a4fd423e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04c0c4c7ca2eb69d11f8",
+    "url": "/static/css/main.4c085dc5.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "04c0c4c7ca2eb69d11f8",
+    "url": "/static/js/main.4b5b1331.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
